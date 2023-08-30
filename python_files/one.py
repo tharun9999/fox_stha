0e57876b9d6e1b5a227ca6d2bc21952ca5e36ffe
tharun gude <tharungude999@gmail.com>
2023-08-30 15:48:08 +0530

    Updated one, two.py files

--- python_files/one.py
+++ python_files/one.py
@@ -1,3 +1,4 @@
 name = input("Enter your name: ")
 
+
 print(f"your name is {name}")
--- python_files/two.py
+++ python_files/two.py
@@ -1,10 +1,12 @@
-#family details
+# family details
 
-details = input("Do you want to store  your family details in database : yes or no :? " ).lower()
+details = input(
+    "Do you want to store  your family details in database : yes or no :? "
+).lower()
 
-while(details != " " or " no") :
-        sir_name = input("Enter your sir name: ")
-        for each_row in range(1,5):
-                name = input("enter your full name")
-                print(f"{sir_name} . {name}")
-                
+while details != " " or " no":
+    sir_name = input("Enter your sir name: ")
+
+    for each_row in range(1, 5):
+        name = input("enter your full name")
+        print(f"{sir_name} . {name}")
