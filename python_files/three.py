7e227b08269d4910ddd68706074d9d26c9d11e31
tharun gude <tharungude999@gmail.com>
2023-08-30 19:51:56 +0530

    New file added only new branch

---
+++ python_files/three.py
@@ -0,0 +1,9 @@
+a = int(input("Enter your first number"))
+b = int(input("Enter your second number"))
+
+c = a + b
+
+if c == (a + b):
+    print("It is correct")
+else:
+    print("It is incorrect")
